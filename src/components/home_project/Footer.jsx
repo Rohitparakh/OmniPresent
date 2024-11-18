9547ba697cf92a41c7c45e038375c42401f3f64b
Rohitparakh <rohitparakh4@gmail.com>
2024-11-18 21:12:57 +0530

    changed marquee copy

--- src/components/home_project/Footer.jsx
+++ src/components/home_project/Footer.jsx
@@ -1,50 +1,37 @@
 import React from 'react';
+import { MarqueeSection } from '../common'
+import right from "../../assets/services/right.svg"
+import marArrow from '../../assets/about/marArrow.svg';
+
 import Marquee from "react-fast-marquee";
 import longArrow from '../../assets/images/longArrow.png';
 import finger from '../../assets/images/fingerHorizontal.png';
 import logo from '../../assets/images/heroFooterLogo.png';
 function Footer() {
     return (
-        <footer className="footer">
-            <div className="footer-content">
-            <Marquee speed={150}>
-                <div className='d-flex jcc aic'>
-            <p className='d-flex jcc aic px-15 fw-300'>
-             <img className='px-15' src={finger} /> <bold>1M+ &nbsp;</bold> community members &nbsp;<bold>($150M+&nbsp; </bold> in client value)
-            </p>
-            <img src={longArrow} className='d-flex jcc aic px-15'/>
-            <p className='d-flex jcc aic'>
-                 partnered with 
-                <img src={logo} alt="icon" className="icon" />
-                 developed for 
-                ( web3 )
-                 for the GROWTH/
-                </p>
-                <img src={longArrow} className='d-flex jcc aic px-15'/>
-            
-                </div>
-                
-                
-                <div className='d-flex jcc aic'>
-            <p className='d-flex jcc aic px-15'>
-            <img className='px-15' src={finger} /> <bold>1M+ &nbsp;</bold> community members &nbsp;<bold>($150M+ &nbsp;</bold> in client value)
+        <div className=' w-full '>
+        <div className='max-w-7xl mx-auto'>
 
-            </p>
-            <img src={longArrow} className='d-flex jcc aic px-15'/>
-            <p className='d-flex jcc aic'>
-                 partnered with 
-                <img src={logo} alt="icon" className="icon" />
-                 developed for 
-                ( web3 )
-                 for the GROWTH/
-                </p>
-                <img src={longArrow} className='d-flex jcc aic px-15'/>
-            
-                </div>
-  </Marquee>
-            
-            </div>
-        </footer>
+        </div>
+
+        <div className='lg:py-6 py-3  w-full'>
+        <MarqueeSection>
+                <h1 className=" text-2xl lg:text-4xl flex tracking-wide font-nohemi items-center gap-4  mx-6 font-semibold select-none">
+              <span className="text-black font-nohemi-ext">5M+</span> social followers
+              <img src={right} className="h-16" alt="" />
+              <span className="text-black font-nohemi-ext">1M+</span>
+              <span className="text-black">community members</span>
+              <span className="text-black font-nohemi-ext">(</span>
+                <span className="text-black font-nohemi-ext">$150M+</span>
+                <span className="text-black">in client venue</span>
+              <span className="text-black font-nohemi-ext">)</span>
+              <img src={marArrow} className="h-20" alt="" />
+    
+            </h1>
+                  </MarqueeSection>
+        </div>
+
+    </div>
     );
 }
 

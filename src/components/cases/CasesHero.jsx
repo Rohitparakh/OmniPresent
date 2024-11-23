d9bdd5887afa8ef5bb5808d740c472a24f46cde1
Rohitparakh <rohitparakh4@gmail.com>
2024-11-23 17:45:06 +0530

    case studies revision2 done

--- src/components/cases/CasesHero.jsx
+++ src/components/cases/CasesHero.jsx
@@ -11,7 +11,7 @@ import x from '../../assets/about/x.svg';
 import telegram from '../../assets/about/telegram.svg';
 import insta from '../../assets/about/insta.svg';
 import { NavLink } from 'react-router-dom';
-
+import blackSquare from '../../assets/images/blackSquare.png'
 const CasesHero = () => {
   return (
     <div className=' w-screen flex flex-col justify-between font-nohemi-ext bg-primary px-4'>
@@ -24,12 +24,12 @@ const CasesHero = () => {
       <div className={`flex flex-col leading-[6rem] max-w-[560px] w-auto items-end justify-end py-1 lg:scale-[1.55] scale-[.6] relative`}>
         <motion.img variants={slideUpDelay}
          initial="hidden"
-         animate="visible" src={pen} alt="" className='absolute top-[-28px] left-[-50px] lg:left-[68px] mix-blend-multiply h-36'/>
+         animate="visible" src={pen} alt="" className='absolute top-[-28px] left-[30px] lg:top-[-24px] lg:left-[-40px] mix-blend-multiply h-36'/>
         <motion.h1 
          variants={slideUp}
          initial="hidden"
          animate="visible"
-        className='text-[10rem] z-10 flex items-end font-nohemi-ext text-black relative'>
+        className='text-[10rem] z-10 flex items-end font-nohemi-ext text-black relative mb-5 lg:mb-0'>
          <span className='text-[10rem] flex items-end w-min pt-9 '></span><span className='font-nohemi-ext'>case</span>
         </motion.h1>
         <motion.h1 
@@ -42,17 +42,20 @@ const CasesHero = () => {
 
         {/* assets for text */}
         <>
-    <div className="absolute top-[-30px] lg:left-[113px] left-[-40px] h-52 border-r-[0.5px] border-black opacity-50 z-50 animate-growY"></div>
+    <div className="absolute top-[-30px] lg:left-[10px] left-[-40px] h-60 border-r-[0.5px] border-black opacity-50 z-50 animate-growY"></div>
 
-    <div className="absolute top-[-30px] lg:left-[207px] left-[70px] h-52 border-r-[0.5px] border-black opacity-50 z-50 animate-growY"></div>
+    <div className="absolute top-[-30px] lg:left-[185px] left-[35px] h-40 lg:h-60 border-r-[0.5px] border-black opacity-50 z-50 animate-growY"></div>
+    <div className="absolute top-[-30px] lg:left-[185px] left-[240px] h-40 lg:h-60 border-r-[0.5px] border-black opacity-50 z-50 animate-growY"></div>
 
-    <div className="absolute lg:right-[-10px] top-[103px] w-[19rem] border-b-[0.5px] border-black opacity-50 z-50 animate-growX"></div>
 
-    <div className="absolute right-[-10px] top-[7px] w-[19.3rem] border-b-[0.5px] border-black opacity-50 animate-growX"></div>
+    <div className="lg:hidden absolute lg:right-[-10px] top-[103px] w-[19rem] border-b-[0.5px] border-black opacity-50 z-50 animate-growX"></div>
 
-    <div className="absolute lg:right-[10px] left-[-50px] top-[7px] w-[33rem] border-b-[0.5px] border-black opacity-50  animate-growX"></div>
+    <div className="lg:hidden absolute right-[-10px] top-[7px] w-[19.3rem] border-b-[0.5px] border-black opacity-50 animate-growX"></div>
 
-    <div className="absolute lg:left-[18px] left-[-55px] bottom-[105px] w-[29rem] border-b-[0.5px] border-black opacity-50 animate-growX"></div>
+    <div className="absolute lg:right-[10px] lg:top-[40px] left-[-50px] top-[7px] w-[33rem] border-b-[0.5px] border-black opacity-50  animate-growX"></div>
+    <div className="absolute lg:right-[10px] lg:top-[40px] left-[-40px] top-[100px] w-[300px] border-b-[0.5px] border-black opacity-50  animate-growX"></div>
+
+    <div className="absolute lg:left-[-50px] left-[-55px] bottom-[105px] w-[35rem] border-b-[0.5px] border-black opacity-50 animate-growX"></div>
 
     <div className="absolute left-[170px] bottom-[8px] w-[22rem] border-b-[0.5px] border-black opacity-50 animate-growX"></div>
 </>
@@ -64,17 +67,17 @@ const CasesHero = () => {
       {/* Center text area end */}
 
       {/* Bottom area start */}
-    <div className='max-w-4xl w-full md:py-5 lg:mt-16 md:mt-10 relative flex lg:flex-row flex-col items-center justify-around gap-2'>
+    <div className='max-w-4xl w-full md:py-5 lg:mt-16 md:mt-10 relative flex lg:flex-row flex-col items-center justify-around gap-12 lg:gap-2'>
         <div className='flex items-center gap-2'>
         <p className=' lg:max-w-[34rem] leading-7 text-xl font-nohemi font-normal'>
-        ■ We’ve fueled growth and built vibrant communities across Web3 for leading brands, including Tier 1 companies and Layer 1 blockchains, and our impact keeps building.
+        <img src={blackSquare} height={20} width={20} style={{display: 'inline-block', marginRight:'5px', marginTop:'-3px'}} /> We’ve fueled growth and built vibrant communities across Web3 for leading brands, including Tier 1 companies and Layer 1 blockchains, and our impact keeps building.
         </p>
         </div>
         <div className='  h-auto lg:pb-20 flex items-start lg:w-auto w-full mb-5'>
-            <Button text="Schedule a call"/>
+            <Button text="schedule a call" arrowBlack={true}/>
         </div>
 
-        <div className='absolute bottom-[-25px] left-[-30px] lg:block hidden'>
+        <div className='absolute bottom-[-25px] left-[-70px] lg:block hidden'>
             <img src={line} className='h-28' alt="" />
         </div>
     </div>
--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { FaArrowRight } from "react-icons/fa";
 import { NavLink } from 'react-router-dom';
+import arrowPrimary from '../../assets/images/arrowPrimary.png'
 
-
-const Button = ({text}) => {
+const Button = ({text, arrowBlack}) => {
   return (
     <NavLink target="_blank" to="https://calendly.com/admin-invictuslabs/30min">
-    <button className=' mt-5 flex items-center justify-center cursor-pointer transition-all duration-300 py-2 bg-primary border border-black rounded-full relative pl-6 pr-7 hover:bg-black text-black hover:text-primary tracking-wide hover:border-primary'>
+    <button className=' mt-5 flex items-center justify-center cursor-pointer transition-all duration-300 py-3 bg-primary border border-black rounded-full relative pl-6 pr-7 hover:bg-black text-black hover:text-primary tracking-wide hover:border-primary'>
         {/* inner text */}
-        <p className=' font-nohemi font-[500] text-2xl'>{text}</p>
+        <p className={` font-nohemi font-[500] text-2xl ${arrowBlack?'mr-2':''}`}>{text}</p>
         {/* outer part */}
-        <div className='w-[2rem] h-[2rem] border-2 border-black rounded-full absolute right-[-12px] bg-primary flex items-center justify-center p-1 hover:border hover:border-primary'>
-            <p className='text-black font-nohemi font-semibold'>
-                <FaArrowRight/>
-            </p>
-        </div>
+        {arrowBlack?
+        <div className='w-[2.5rem] h-[2.5rem] border-2 border-black rounded-full absolute right-[-12px] bg-black flex items-center justify-center p-2 hover:border hover:border-primary'>
+        <p className='text-black font-nohemi font-semibold'>
+            <img src={arrowPrimary} height={20}/>
+        </p>
+    </div>:
+    <div className='w-[2rem] h-[2rem] border-2 border-black rounded-full absolute right-[-12px] bg-primary flex items-center justify-center p-1 hover:border hover:border-primary'>
+    <p className='text-black font-nohemi font-semibold'>
+        <FaArrowRight/>
+    </p>
+</div>
+        }
+        
     </button>
     </NavLink>
   )
